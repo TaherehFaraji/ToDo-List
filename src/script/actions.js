aa0001aef8a94b15232b889478f757abd0f984c8
Tahereh Faraji <taherehfaraji100@gmail.com>
2026-02-24 20:47:21 +0330

    finish

--- src/script/actions.js
+++ src/script/actions.js
@@ -8,17 +8,17 @@ addbtn.addEventListener("click", () => {
         const li = document.createElement("li");
         li.innerHTML = `
         <p>${task}</p>
-        <button id="Sbtn" type="button" class="btn btn-outline-success">Success</button>
-        <button id="Cbtn" type="button" class="btn btn-outline-danger">Cancel</button>
+        <button type="button" class="btn btn-outline-success Sbtn">Success</button>
+        <button type="button" class="btn btn-outline-danger Cbtn">Cancel</button>
         `;
         list.appendChild(li);
 
-        const successBtn = document.getElementById("Sbtn");
+        const successBtn = li.querySelector(".Sbtn");
         successBtn.addEventListener("click", (e) => {
             e.target.closest("li").style.color = "green";
         });
 
-        const cancelBtn = document.getElementById("Cbtn");
+        const cancelBtn = li.querySelector(".Cbtn");
         cancelBtn.addEventListener("click", (e) => {
             e.target.closest("li").remove();
         });
